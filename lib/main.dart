d87590f9f3922ffd75ef810a0930f0c0f60bcdd0
lijo <lijo.joseph@allianz.co.id>
2020-03-01 17:39:02 +0700

    Fix: Map Sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,249 +1,143 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:map_view/figure_joint_type.dart';
-import 'package:map_view/map_view.dart';
-import 'package:map_view/polygon.dart';
-import 'package:map_view/polyline.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-///This API Key will be used for both the interactive maps as well as the static maps.
-///Make sure that you have enabled the following APIs in the Google API Console (https://console.developers.google.com/apis)
-/// - Static Maps API
-/// - Android Maps API
-/// - iOS Maps API
-const API_KEY = "AIzaSyBYtUqqy5mHzbA0tTIrYaNV19D3aJbIyEE";
+void main() => runApp(MyApp());
 
-void main() {
-  MapView.setApiKey(API_KEY);
-  runApp(new MyApp());
-}
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  MapView mapView = new MapView();
-  CameraPosition cameraPosition;
-  var compositeSubscription = new CompositeSubscription();
-  var staticMapProvider = new StaticMapProvider(API_KEY);
-  Uri staticMapUri;
-
-  //Marker bubble
-  List<Marker> _markers = <Marker>[
-    new Marker(
-      "1",
-      "Something fragile!",
-      45.52480841512737,
-      -122.66201455146073,
-      color: Colors.blue,
-      draggable: true, //Allows the user to move the marker.
-      markerIcon: new MarkerIcon(
-        "images/flower_vase.png",
-        width: 112.0,
-        height: 75.0,
-      ),
-    ),
-  ];
-
-  //Line
-  List<Polyline> _lines = <Polyline>[
-    new Polyline(
-        "11",
-        <Location>[
-          new Location(45.52309483308097, -122.67339684069155),
-          new Location(45.52298442915803, -122.66339991241693),
-        ],
-        width: 15.0,
-        color: Colors.blue),
-  ];
-
-  
-
-  @override
-  initState() {
-    super.initState();
-    cameraPosition = new CameraPosition(Locations.portland, 2.0);
-    staticMapUri = staticMapProvider.getStaticUri(Locations.portland, 12,
-        width: 900, height: 400, mapType: StaticMapViewType.roadmap);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
   }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  MapType _currentMapType = MapType.normal;
+  final Set<Marker> _markers = {};
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  LatLng _lastMapPosition = _center;
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Map View Example'),
-          ),
-          body: new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                height: 250.0,
-                child: new Stack(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              mapType: _currentMapType,
+              markers: _markers,
+              onCameraMove: _onCameraMove,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Column(
                   children: <Widget>[
-                    new Center(
-                        child: new Container(
-                      child: new Text(
-                        "You are supposed to see a map here.\n\nAPI Key is not valid.\n\n"
-                            "To view maps in the example application set the "
-                            "API_KEY variable in example/lib/main.dart. "
-                            "\n\nIf you have set an API Key but you still see this text "
-                            "make sure you have enabled all of the correct APIs "
-                            "in the Google API Console. See README for more detail.",
-                        textAlign: TextAlign.center,
-                      ),
-                      padding: const EdgeInsets.all(20.0),
-                    )),
-                    new InkWell(
-                      child: new Center(
-                        child: new Image.network(staticMapUri.toString()),
-                      ),
-                      onTap: showMap,
-                    )
+                    FloatingActionButton(
+                      onPressed: _onMapTypeButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(Icons.map, size: 36.0),
+                    ),
+                    SizedBox(height: 16.0),
+                    FloatingActionButton(
+                      onPressed: _onAddMarkerButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(Icons.add_location, size: 36.0),
+                    ),
                   ],
                 ),
               ),
-              new Container(
-                padding: new EdgeInsets.only(top: 10.0),
-                child: new Text(
-                  "Tap the map to interact",
-                  style: new TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              new Container(
-                padding: new EdgeInsets.only(top: 25.0),
-                child:
-                    new Text("Camera Position: \n\nLat: ${cameraPosition.center
-                    .latitude}\n\nLng:${cameraPosition.center
-                    .longitude}\n\nZoom: ${cameraPosition.zoom}"),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  showMap() {
-    mapView.show(
-        new MapOptions(
-            mapViewType: MapViewType.normal,
-            showUserLocation: true,
-            showMyLocationButton: true,
-            showCompassButton: true,
-            initialCameraPosition: new CameraPosition(
-                new Location(45.526607443935724, -122.66731660813093), 15.0),
-            hideToolbar: false,
-            title: "Recently Visited"),
-        toolbarActions: [new ToolbarAction("Close", 1)]);
-    StreamSubscription sub = mapView.onMapReady.listen((_) {
-      mapView.setMarkers(_markers);
-      mapView.setPolylines(_lines);
-      //\mapView.setPolygons(_polygons);
+  void _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
     });
-    compositeSubscription.add(sub);
-    sub = mapView.onLocationUpdated.listen((location) {
-      print("Location updated $location");
-    });
-    compositeSubscription.add(sub);
-    sub = mapView.onTouchAnnotation
-        .listen((annotation) => print("annotation ${annotation.id} tapped"));
-    compositeSubscription.add(sub);
-    sub = mapView.onTouchPolyline
-        .listen((polyline) => print("polyline ${polyline.id} tapped"));
-    compositeSubscription.add(sub);
-    sub = mapView.onTouchPolygon
-        .listen((polygon) => print("polygon ${polygon.id} tapped"));
-    compositeSubscription.add(sub);
-    sub = mapView.onMapTapped
-        .listen((location) => print("Touched location $location"));
-    compositeSubscription.add(sub);
-    sub = mapView.onMapLongTapped
-        .listen((location) => print("Long tapped location $location"));
-    compositeSubscription.add(sub);
-    sub = mapView.onCameraChanged.listen((cameraPosition) =>
-        this.setState(() => this.cameraPosition = cameraPosition));
-    compositeSubscription.add(sub);
-    sub = mapView.onAnnotationDragStart.listen((markerMap) {
-      var marker = markerMap.keys.first;
-      print("Annotation ${marker.id} dragging started");
-    });
-    sub = mapView.onAnnotationDragEnd.listen((markerMap) {
-      var marker = markerMap.keys.first;
-      print("Annotation ${marker.id} dragging ended");
-    });
-    sub = mapView.onAnnotationDrag.listen((markerMap) {
-      var marker = markerMap.keys.first;
-      var location = markerMap[marker];
-      print("Annotation ${marker.id} moved to ${location.latitude} , ${location
-          .longitude}");
-    });
-    compositeSubscription.add(sub);
-    sub = mapView.onToolbarAction.listen((id) {
-      print("Toolbar button id = $id");
-      if (id == 1) {
-        _handleDismiss();
-      }
-    });
-    compositeSubscription.add(sub);
-    sub = mapView.onInfoWindowTapped.listen((marker) {
-      print("Info Window Tapped for ${marker.title}");
-    });
-    compositeSubscription.add(sub);
-    sub = mapView.onIndoorBuildingActivated.listen(
-        (indoorBuilding) => print("Activated indoor building $indoorBuilding"));
-    compositeSubscription.add(sub);
-    sub = mapView.onIndoorLevelActivated.listen(
-        (indoorLevel) => print("Activated indoor level $indoorLevel"));
-    compositeSubscription.add(sub);
   }
 
-  _handleDismiss() async {
-    double zoomLevel = await mapView.zoomLevel;
-    Location centerLocation = await mapView.centerLocation;
-    List<Marker> visibleAnnotations = await mapView.visibleAnnotations;
-    List<Polyline> visibleLines = await mapView.visiblePolyLines;
-    List<Polygon> visiblePolygons = await mapView.visiblePolygons;
-    print("Zoom Level: $zoomLevel");
-    print("Center: $centerLocation");
-    print("Visible Annotation Count: ${visibleAnnotations.length}");
-    print("Visible Polylines Count: ${visibleLines.length}");
-    print("Visible Polygons Count: ${visiblePolygons.length}");
-    var uri = await staticMapProvider.getImageUriFromMap(mapView,
-        width: 900, height: 400);
-    setState(() => staticMapUri = uri);
-    mapView.dismiss();
-    compositeSubscription.cancel();
-  }
-}
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
-class CompositeSubscription {
-  Set<StreamSubscription> _subscriptions = new Set();
+  // @override
+  // Widget build(BuildContext context) {
+  //   return new Scaffold(
+  //     body: GoogleMap(
+  //       mapType: MapType.normal,
+  //       initialCameraPosition: _kGooglePlex,
+  //       onMapCreated: (GoogleMapController controller) {
+  //         _controller.complete(controller);
+  //       },
+  //     ),
+  //     floatingActionButton: FloatingActionButton.extended(
+  //       onPressed: _goToTheLake,
+  //       label: Text('To the lake!'),
+  //       icon: Icon(Icons.directions_boat),
+  //     ),
+  //   );
+  // }
 
-  void cancel() {
-    for (var n in this._subscriptions) {
-      n.cancel();
-    }
-    this._subscriptions = new Set();
-  }
-
-  void add(StreamSubscription subscription) {
-    this._subscriptions.add(subscription);
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 
-  void addAll(Iterable<StreamSubscription> subs) {
-    _subscriptions.addAll(subs);
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'Really cool place',
+          snippet: '5 Star Rating',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
   }
 
-  bool remove(StreamSubscription subscription) {
-    return this._subscriptions.remove(subscription);
-  }
-
-  bool contains(StreamSubscription subscription) {
-    return this._subscriptions.contains(subscription);
-  }
-
-  List<StreamSubscription> toList() {
-    return this._subscriptions.toList();
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
   }
 }
